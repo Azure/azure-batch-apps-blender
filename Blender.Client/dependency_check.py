00e5eaf8855c48bd7bb0ca0d98681e4e6583aec0
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-03 01:05:03 +0000

    Adding tarfile member sanitization to extractall()

--- Blender.Client/dependency_check.py
+++ Blender.Client/dependency_check.py
@@ -72,7 +72,26 @@ def unpack_tar(lib_full, lib_dir, lib_module):
         members = tfile.getmembers()
         mod_path = "{0}/{1}/".format(lib_dir, lib_module)
         subset = [m for m in members if m.path.startswith(mod_path)]
-        tfile.extractall(path=TEMP_DIR, members=subset)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tfile, path=TEMP_DIR, members=subset)
 
 def unpack_zip(lib_full, lib_dir, lib_module):
 
