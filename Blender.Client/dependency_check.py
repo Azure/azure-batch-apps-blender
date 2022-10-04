c20f1cdfd1f90cc30a8e280f218a06c1d1981b8f
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 21:07:10 +0000

    Adding numeric_owner as keyword arguement

--- Blender.Client/dependency_check.py
+++ Blender.Client/dependency_check.py
@@ -88,7 +88,7 @@ def unpack_tar(lib_full, lib_dir, lib_module):
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(tfile, path=TEMP_DIR, members=subset)
